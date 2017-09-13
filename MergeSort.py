696d86691d05c8b3b725060d910f406aa6245f0b
jrhilke <jrhilke@mit.edu>
2017-09-12 22:49:56 -0400

    remove MergSort.py

--- MergeSort.py
+++
@@ -1,9 +0,0 @@
-def MergeSort(A):
-    if len(A)==1:
-        return A
-    n = len(A)
-
-    first_half=A[:n/2]
-    second_half=A[n/2:]
-    return MergeSort(A[:n/2])+MergeSort(A[n/2:])
-A = [1,5,3,6,7,2,10]
